00a80f6f078f50b6273f74d41bc4d47bf8e4ef08
Khadath <corentin.chignin@hotmail.fr>
2020-12-03 18:14:01 +0100

    Add file .sh & .yml

---
+++ .travis.deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+
+echo -e "\033[0;32mDeploying LirisProjet to rict...\033[0m"
+cd $TRAVIS_BUILD_DIR
+ssh -i $TRAVIS_BUILD_DIR/.ssh/id_rsa.vcity_rict connect.liris.cnrs.fr ls /home-projets/vcity/public_html
