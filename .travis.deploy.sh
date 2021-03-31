98413abc1d1753f3deccbfd39e32487e7a94887d
Corentin GAUTIER <32339907+CorentinGaut@users.noreply.github.com>
2021-03-31 14:53:54 +0200

    Update .travis.deploy.sh

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -3,4 +3,4 @@ set -x
 
 echo -e "\033[0;32mDeploying LirisProjet to rict...\033[0m"
 cd $TRAVIS_BUILD_DIR
-ssh -i $TRAVIS_BUILD_DIR/.ssh/id_rsa.vcity_rict connect.liris.cnrs.fr ls /home-projets/vcity/public_html/DatAgora
+ssh -i $TRAVIS_BUILD_DIR/.ssh/id_rsa.vcity_rict.enc connect.liris.cnrs.fr ls /home-projets/vcity/public_html/DatAgora
